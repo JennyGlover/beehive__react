a6db52e698f6068e34ad91b95316f4b1339f0875
Jenny Saunders <jennygloversaunders@gmail.com>
2025-01-19 22:21:10 -0500

    style: finish styling nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -9,13 +9,12 @@ function Navigation (){
     <div className='Navigation__logo-container'>
     <img className="Navigation__logo" src={logo} alt="BeeHive logo"/>
     <p className="Navigation__logo-text">BeeHive</p>
-    <p className="Navigation__logo-message">That's my honey!</p>
     </div>
     <hr className="Navigation__line"></hr> 
-    <p className='Navigation__menu-text'>Menu</p>
+    <p className='Navigation__menu-text'>Menu </p>
     <div className='Navigation__page-Links'>
-    <p className='Navigation__page-link'>👯‍♀️ Pals</p>
-    <p className='Navigation__page-link'>💬 Buzz</p>
+    <p className='Navigation__page-link'>👯‍♀️ Pals <span className="Navigation__link-counter">0</span></p>
+    <p className='Navigation__page-link Navigation__buzz-link'>💬 Buzz <span className="Navigation__link-counter">0</span></p>
     </div>
     <hr className="Navigation__line"></hr> 
     <p className='Navigation__page-link Navigation__profile-Modal-link '>🐝 Profile</p>
