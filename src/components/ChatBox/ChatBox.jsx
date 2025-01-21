ea1375a45775ff345baa532d095a5810981399d6
Jenny Saunders <jennygloversaunders@gmail.com>
2025-01-21 03:32:56 -0500

    style: add time component

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,18 +1,18 @@
 import ChatCard from "../ChatCard/ChatCard";
 import ImageCard from "../ImageCard/ImageCard";
 import MusicCard from "../MusicCard/MusicCard";
+import TimeSent from "../TimeSent/TimeSent";
+
 import "./ChatBox.css";
 
 function ChatBox() {
   return (
     <div className="ChatBox">
       <div className="ChatBox__container">
-        <MusicCard position="left" />
         <MusicCard position="right" />
-        <ChatCard position="left" />
         <ChatCard position="right" />
         <ImageCard position="right" />
-        <ImageCard position="left" />
+        <TimeSent position="right"/>
       </div>
     </div>
   );
---
+++ src/components/TimeSent/TimeSent.jsx
@@ -0,0 +1,10 @@
+import './TimeSent.css';
+
+function TimeSent ({ position }) {
+
+    return(
+        <div className={`TimeSent TimeSent__position-${position}`}>Today 10:00PM</div>
+    );
+}
+
+export default TimeSent;
